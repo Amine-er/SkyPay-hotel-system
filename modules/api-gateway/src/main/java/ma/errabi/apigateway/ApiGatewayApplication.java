af4a1a9745b44f454032880b4edbaa342e0e6b6a
Errabi Mohamed Amine <errabi.ami@gmail.com>
2025-06-24 23:38:20 +0100

    initialize project with Gradle, Spring Boot, and basic configurations

---
+++ modules/api-gateway/src/main/java/ma/errabi/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,10 @@
+package ma.errabi.apigateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
