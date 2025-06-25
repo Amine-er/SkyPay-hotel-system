615c0141352410cdfffd45b223d3a96c62ead745
Errabi Mohamed Amine <errabi.ami@gmail.com>
2025-06-25 11:07:24 +0100

    Add user service module with configuration and dependencies

---
+++ modules/user-service/src/main/java/ma/errabi/user/UserServiceApplication.java
@@ -0,0 +1,10 @@
+package ma.errabi.user;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
