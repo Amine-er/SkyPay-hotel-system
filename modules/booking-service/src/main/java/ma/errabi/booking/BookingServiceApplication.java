e3e7e2c93f948bb2080c553e7eca9a714f395e20
Errabi Mohamed Amine <errabi.ami@gmail.com>
2025-06-25 11:33:59 +0100

    Add booking service module with configuration and dependencies

---
+++ modules/booking-service/src/main/java/ma/errabi/booking/BookingServiceApplication.java
@@ -0,0 +1,10 @@
+package ma.errabi.booking;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServiceApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(BookingServiceApplication.class, args);
+    }
+}
