a17b112719a15f4c16f750b97e9fc72e5f7b5a83
Errabi Mohamed Amine <errabi.ami@gmail.com>
2025-06-25 11:17:42 +0100

    Add room service module with configuration and dependencies

---
+++ modules/room-service/src/main/java/ma/errabi/room/RoomServiceApplication.java
@@ -0,0 +1,10 @@
+package ma.errabi.room;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomServiceApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(RoomServiceApplication.class, args);
+    }
+}
