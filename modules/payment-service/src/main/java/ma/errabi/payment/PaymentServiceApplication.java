c99fa75dea441db9bac31ee65ed8e84e22d8d56e
Errabi Mohamed Amine <errabi.ami@gmail.com>
2025-06-25 11:44:17 +0100

    Add payment service module with configuration and dependencies

---
+++ modules/payment-service/src/main/java/ma/errabi/payment/PaymentServiceApplication.java
@@ -0,0 +1,10 @@
+package ma.errabi.payment;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(PaymentServiceApplication.class, args);
+    }
+}
